46bdac70b9e332a83e3e6c30c9474926ed05f7ab
jpgaertner <jan.gaertner.l@gmail.com>
2021-12-16 21:58:36 +0100

    fix getforcing

--- get_forcings.py
+++ get_forcings.py
@@ -1,11 +1,11 @@
 import numpy as np
 
-path = "C:/Users/Jan/Documents/MITgcm/verification/1D_ocean_ice_column/input/"
-AirTemp = np.fromfile(path + "atemp_1x1_one_year")
-LWDown = np.fromfile(path + "dlwrf_1x1_one_year")
-SWDown = np.fromfile(path + "dswrf_1x1_one_year")
-uWind = np.fromfile(path + "u_1ms_1x1_one_year")
-vWind = np.fromfile(path + "u_1ms_1x1_one_year")
+path = "/Users/jgaertne/MITgcm/verification/1D_ocean_ice_column/input/"
+AirTemp = np.fromfile(path + "atemp_1x1_one_year", dtype = ">f4")
+LWDown = np.fromfile(path + "dlwrf_1x1_one_year", dtype = ">f4")
+SWDown = np.fromfile(path + "dswrf_1x1_one_year", dtype = ">f4")
+uWind = np.fromfile(path + "u_1ms_1x1_one_year", dtype = ">f4")
+vWind = np.fromfile(path + "u_1ms_1x1_one_year", dtype = ">f4")
 
 
 print(f"AirTemp{AirTemp[:10]}")
