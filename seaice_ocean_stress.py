e7df49d63c3d5373774c3f3b76556818ca79466b
jpgaertner <jan.gaertner.l@gmail.com>
2022-02-02 09:37:47 +0100

    comment changes

--- seaice_ocean_stress.py
+++ seaice_ocean_stress.py
@@ -8,17 +8,17 @@ from seaice_ocean_drag_coeffs import ocean_drag_coeffs
 
 
 ### input
-# uIce: zonal ice velocity - given at the u point?
+# uIce: zonal ice velocity
 # vIce: meridional ice velocity
 # uVel: zonal ocean velocity
 # vVel: meridional ice velocity
 # Area: ice cover fraction
-# fu: zonal ice-ocean stress
-# fv: meridional ice-ocean stress
+# fu: zonal stress on ocean surface (ice or atmopshere)
+# fv: meridional stress on ocean surface (ice or atmopshere)
 
 ### output
-# fu: zonal ice-ocean stress
-# fv: meridional ice-ocean stress
+# fu: zonal stress on ocean surface (ice or atmopshere)
+# fv: meridional stress on ocean surface (ice or atmopshere)
 
 
 def ocean_stress(uIce, vIce, uVel, vVel, Area, fu, fv):
@@ -35,7 +35,7 @@ def ocean_stress(uIce, vIce, uVel, vVel, Area, fu, fv):
     fvLoc = 0.5 * (cDrag[OLy:-OLy,OLx:-OLx] + cDrag[OLy-1:-OLy-1,OLx:-OLx]) * cosWat * (vIce[OLy:-OLy,OLx:-OLx] - vVel[OLy:-OLy,OLx:-OLx]) + np.sign(fCori[OLy:-OLy,OLx:-OLx]) * sinWat * 0.5 * (cDrag[OLy:-OLy,OLx:-OLx] * 0.5 * (uIce[OLy:-OLy,OLx:-OLx] - uVel[OLy:-OLy,OLx:-OLx] + uIce[OLy:-OLy,OLx+1:-OLx+1] - uVel[OLy:-OLy,OLx+1:-OLx+1]) + cDrag[OLy:-OLy,OLx:-OLx] * 0.5 * (uIce[OLy-1:-OLy-1,OLx:-OLx] - uVel[OLy-1:-OLy-1,OLx:-OLx] + uIce[OLy-1:-OLy-1,OLx+1:-OLx+1] - uVel[OLy-1:-OLy-1,OLx+1:-OLx+1]))
     areaW = 0.5 * (Area[OLy:-OLy,OLx:-OLx] + Area[OLy:-OLy,OLx-1:-OLx-1]) * stressFactor #stressFactor = 1, what is it, is it needed? 
     areaS = 0.5 * (Area[OLy:-OLy,OLx:-OLx] + Area[OLy-1:-OLy-1,OLx:-OLx]) * stressFactor
-    fu[OLy:-OLy,OLx:-OLx] = (1 - areaW) * fu[OLy:-OLy,OLx:-OLx] + areaW * fuLoc   #is fu, fv an input (from previous iteration)?
+    fu[OLy:-OLy,OLx:-OLx] = (1 - areaW) * fu[OLy:-OLy,OLx:-OLx] + areaW * fuLoc
     fv[OLy:-OLy,OLx:-OLx] = (1 - areaS) * fv[OLy:-OLy,OLx:-OLx] + areaS * fvLoc
 
     # fill overlaps
