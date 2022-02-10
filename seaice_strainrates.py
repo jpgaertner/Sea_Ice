48bf92ece733ee70ec8156a02f00f71493dc7cf9
Martin Losch <martin.losch@awi.de>
2022-02-10 15:31:21 +0100

    unecessary: rewrite 2nd order boundary conditions

--- seaice_strainrates.py
+++ seaice_strainrates.py
@@ -46,23 +46,32 @@ def strainrates(uFld, vFld, secondOrderBC):
             + 2.0 * vave * recip_dxV * hFacV
         )
 
-    if secondOrderBC: print('not implemented yet')
-    # if secondOrderBC:
-    #     hFacU = (SeaIceMaskU[2:-1,2:-1] - SeaIceMaskU[1:-2,2:-1]) / 3.
-    #     hFacV = (SeaIceMaskV[2:-1,2:-1] - SeaIceMaskV[2:-1,1:-2]) / 3.
-    #     hFacU = hFacU * (SeaIceMaskU[:-3,2:-1] * SeaIceMaskU[1:-2,2:-1]
-    #                      + SeaIceMaskU[3:,2:-1] * SeaIceMaskU[2:-1,2:-1])
-    #     hFacV = hFacV * (SeaIceMaskV[2:-1,:-3] * SeaIceMaskV[2:-1,1:-2]
-    #                      + SeaIceMaskV[2:-1,3:] * SeaIceMaskV[2:-1,2:-1])
-
-    #     e12[2:-1,2:-1] = e12[2:-1,2:-1] + 0.5 * (
-    #         recip_dyU[2:-1,2:-1] * (6 * uave[2:-1,2:-1]
-    #                                 - uFld[:-3,2:-1] * SeaIceMaskU[1:-2,2:-1]
-    #                                 - uFld[3:,2:-1] * SeaIceMaskU[2:-1,2:-1]
-    #                                 ) * hFacU \
-    #         + recip_dxV[2:-1,2:-1] * (
-    #             6 * vave[2:-1,2:-1]
-    #             - vFld[2:-1,:-3] * SeaIceMaskV[2:-1,1:-2]
-    #             - vFld[2:-1,3:] * SeaIceMaskV[2:-1,2:-1]) * hFacV)
+    if secondOrderBC:
+        hFacU = ( SeaIceMaskU - np.roll(SeaIceMaskU,1,0) ) / 3.
+        hFacV = ( SeaIceMaskV - np.roll(SeaIceMaskV,1,1) ) / 3.
+        hFacU = hFacU * (np.roll(SeaIceMaskU, 2,0) * np.roll(SeaIceMaskU,1,0)
+                       + np.roll(SeaIceMaskU,-1,0) * SeaIceMaskU )
+        hFacV = hFacV * (np.roll(SeaIceMaskV, 2,1) * np.roll(SeaIceMaskV,1,1)
+                       + np.roll(SeaIceMaskV,-1,1) * SeaIceMaskV )
+        # right hand sided dv/dx = (9*v(i,j)-v(i+1,j))/(4*dxv(i,j)-dxv(i+1,j))
+        # according to a Taylor expansion to 2nd order. We assume that dxv
+        # varies very slowly, so that the denominator simplifies to 3*dxv(i,j),
+        # then dv/dx = (6*v(i,j)+3*v(i,j)-v(i+1,j))/(3*dxv(i,j))
+        #            = 2*v(i,j)/dxv(i,j) + (3*v(i,j)-v(i+1,j))/(3*dxv(i,j))
+        # the left hand sided dv/dx is analogously
+        #            = - 2*v(i-1,j)/dxv(i,j)-(3*v(i-1,j)-v(i-2,j))/(3*dxv(i,j))
+        # the first term is the first order part, which is already added.
+        # For e12 we only need 0.5 of this gradient and vave = is either
+        # 0.5*v(i,j) or 0.5*v(i-1,j) near the boundary so that we need an
+        # extra factor of 2. This explains the six. du/dy is analogous.
+        # The masking is ugly, but hopefully effective.
+        e12 = e12 + 0.5 * (
+            recip_dyU * ( 6. * uave
+                          - np.roll(uFld, 2,0) * np.roll(SeaIceMaskU,1,0)
+                          - np.roll(uFld,-1,0) * SeaIceMaskU ) * hFacU
+          + recip_dxV * ( 6. * vave
+                          - np.roll(vFld, 2,1) * np.roll(SeaIceMaskV,1,1)
+                          - np.roll(vFld,-1,1) * SeaIceMaskV ) * hFacV
+        )
 
     return e11, e22, e12
