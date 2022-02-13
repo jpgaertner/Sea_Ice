1e8cef1f99884f93a4a4328f8fa17f31dbb58379
Martin Losch <martin.losch@awi.de>
2022-02-13 23:50:18 +0100

    small adjustments

--- seaice_viscosities.py
+++ seaice_viscosities.py
@@ -12,26 +12,26 @@
 """
 import numpy as np
 
-from seaice_params import *
+from seaice_params import PlasDefCoeff, deltaMin, tensileStrFac, pressReplFac
 from seaice_size import *
 
 def viscosities(e11,e22,e12,press0,iStep,myTime,myIter):
 
     recip_PlasDefCoeffSq = 1. / PlasDefCoeff**2
-    ep = e11 + e22
-    em = e11 - e22
 
     # use area weighted average of squares of e12 (more accurate)
     e12Csq = rAz * e12**2
     e12Csq =                     e12Csq + np.roll(e12Csq,-1,0)
     e12Csq = 0.25 * recip_rA * ( e12Csq + np.roll(e12Csq,-1,1) )
 
-    deltaSq = ep**2 + recip_PlasDefCoeffSq * ( em**2 + 4. * e12Csq )
+    deltaSq = (e11+e22)**2 + recip_PlasDefCoeffSq * (
+        (e11-e22)**2 + 4. * e12Csq )
     deltaC = np.sqrt(deltaSq)
 
     # smooth regularization of delta for better differentiability
     deltaCreg = deltaC + deltaMin
     # deltaCreg = np.sqrt( deltaSq + deltaMin**2 )
+    # deltaCreg = np.maximum(deltaC,deltaMin)
 
     zeta = 0.5 * (press0 * (1 + tensileStrFac)) / deltaCreg
     eta  = zeta * recip_PlasDefCoeffSq
