5376102cc475f808f01face2bd7b31e6e0c54fea
Martin Losch <martin.losch@awi.de>
2022-02-16 09:48:52 +0100

    cleanup, also following up on changes to ocean_drag_coeffs

--- seaice_ocean_stress.py
+++ seaice_ocean_stress.py
@@ -3,25 +3,23 @@ import numpy as np
 from seaice_params import *
 from seaice_size import *
 
-from seaice_fill_overlap import fill_overlap
-from seaice_ocean_drag_coeffs import ocean_drag_coeffs
-
-
-### input
-# uIce: zonal ice velocity
-# vIce: meridional ice velocity
-# uVel: zonal ocean velocity
-# vVel: meridional ice velocity
-# Area: ice cover fraction
-# fu: zonal stress on ocean surface (ice or atmopshere)
-# fv: meridional stress on ocean surface (ice or atmopshere)
-
-### output
-# fu: zonal stress on ocean surface (ice or atmopshere)
-# fv: meridional stress on ocean surface (ice or atmopshere)
-
+from seaice_fill_overlap import fill_overlap_uv
+from dynamics_routines import ocean_drag_coeffs
 
 def ocean_stress(uIce, vIce, uVel, vVel, Area, fu, fv):
+
+    ### input
+    # uIce: zonal ice velocity
+    # vIce: meridional ice velocity
+    # uVel: zonal ocean velocity
+    # vVel: meridional ice velocity
+    # Area: ice cover fraction
+    # fu: zonal stress on ocean surface (ice or atmopshere)
+    # fv: meridional stress on ocean surface (ice or atmopshere)
+
+    ### output
+    # fu: zonal stress on ocean surface (ice or atmopshere)
+    # fv: meridional stress on ocean surface (ice or atmopshere)
 
     # get linear drag coefficient at c point
     cDrag = ocean_drag_coeffs(uIce, vIce, uVel, vVel)
@@ -30,17 +28,26 @@ def ocean_stress(uIce, vIce, uVel, vVel, Area, fu, fv):
     sinWat = np.sin(np.deg2rad(waterTurnAngle))
     cosWat = np.cos(np.deg2rad(waterTurnAngle))
 
-    # calculate ice affected wind stress by averaging wind stress and ice-ocean stress according to ice cover
-    fuLoc = 0.5 * (cDrag[OLy:-OLy,OLx:-OLx] + cDrag[OLy:-OLy,OLx+1:-OLx+1]) * cosWat * (uIce[OLy:-OLy,OLx:-OLx] - uVel[OLy:-OLy,OLx:-OLx]) - np.sign(fCori[OLy:-OLy,OLx:-OLx]) * sinWat * 0.5 * (cDrag[OLy:-OLy,OLx:-OLx] * 0.5 * (vIce[OLy:-OLy,OLx:-OLx] - vVel[OLy:-OLy,OLx:-OLx] + vIce[OLy+1:-OLy+1,OLx:-OLx] - vVel[OLy+1:-OLy+1,OLx:-OLx]) + cDrag[OLy:-OLy,OLx:-OLx] * 0.5 * (vIce[OLy:-OLy,OLx-1:-OLx-1] - vVel[OLy:-OLy,OLx-1:-OLx-1] + vIce[OLy+1:-OLy+1,OLx-1:-OLx-1] - vVel[OLy+1:-OLy+1,OLx-1:-OLx-1]))
-    fvLoc = 0.5 * (cDrag[OLy:-OLy,OLx:-OLx] + cDrag[OLy-1:-OLy-1,OLx:-OLx]) * cosWat * (vIce[OLy:-OLy,OLx:-OLx] - vVel[OLy:-OLy,OLx:-OLx]) + np.sign(fCori[OLy:-OLy,OLx:-OLx]) * sinWat * 0.5 * (cDrag[OLy:-OLy,OLx:-OLx] * 0.5 * (uIce[OLy:-OLy,OLx:-OLx] - uVel[OLy:-OLy,OLx:-OLx] + uIce[OLy:-OLy,OLx+1:-OLx+1] - uVel[OLy:-OLy,OLx+1:-OLx+1]) + cDrag[OLy:-OLy,OLx:-OLx] * 0.5 * (uIce[OLy-1:-OLy-1,OLx:-OLx] - uVel[OLy-1:-OLy-1,OLx:-OLx] + uIce[OLy-1:-OLy-1,OLx+1:-OLx+1] - uVel[OLy-1:-OLy-1,OLx+1:-OLx+1]))
-    areaW = 0.5 * (Area[OLy:-OLy,OLx:-OLx] + Area[OLy:-OLy,OLx-1:-OLx-1]) * stressFactor #stressFactor = 1, what is it, is it needed? 
-    areaS = 0.5 * (Area[OLy:-OLy,OLx:-OLx] + Area[OLy-1:-OLy-1,OLx:-OLx]) * stressFactor
-    fu[OLy:-OLy,OLx:-OLx] = (1 - areaW) * fu[OLy:-OLy,OLx:-OLx] + areaW * fuLoc
-    fv[OLy:-OLy,OLx:-OLx] = (1 - areaS) * fv[OLy:-OLy,OLx:-OLx] + areaS * fvLoc
+    # calculate ice affected wind stress by averaging wind stress and
+    # ice-ocean stress according to ice cover
+    du = uIce - uVel
+    dv = vIce - vVel
+    duAtC = 0.5 * (du + np.roll(du,-1,1))
+    dvAtC = 0.5 * (dv + np.roll(dv,-1,0))
+    fuLoc = 0.5 * (cDrag + np.roll(cDrag,1,1)) * cosWat * du \
+        - np.sign(fCori) * sinWat * 0.5 * (
+            cDrag * dvAtC + np.roll(cDrag * dvAtC,1,0) )
+    fvLoc = 0.5 * (cDrag + np.roll(cDrag,1,0)) * cosWat * dv \
+        + np.sign(fCori) * sinWat * 0.5 * (
+            cDrag * duAtC + np.roll(cDrag * duAtC,1,1) )
+
+    #stressFactor = 1, what is it, is it needed?
+    areaW = 0.5 * (Area + np.roll(Area,1,1)) * stressFactor
+    areaS = 0.5 * (Area + np.roll(Area,1,0)) * stressFactor
+    fu = (1 - areaW) * fu + areaW * fuLoc
+    fv = (1 - areaS) * fv + areaS * fvLoc
 
     # fill overlaps
-    fu = fill_overlap(fu)
-    fv = fill_overlap(fv)
-
+    fu, fv = fill_overlap_uv(fu,fv)
 
     return fu, fv
