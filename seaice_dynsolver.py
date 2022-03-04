b74e6a8d49d2d882a20956b1d4bfb6c66733bda9
jpgaertner <jan.gaertner.l@gmail.com>
2022-03-04 21:32:28 +0100

    use VerosState object and veros_kernel/ -routine

--- seaice_dynsolver.py
+++ seaice_dynsolver.py
@@ -1,123 +1,107 @@
 from veros.core.operators import numpy as npx
+from veros import veros_routine, veros_kernel, KernelOutput
 
-from seaice_size import *
-from seaice_params import *
+from seaice_params import rhoIce, rhoSnow, useRealFreshWaterFlux, \
+                            seaIceLoadFac, recip_rhoConst, gravity, \
+                            useFreedrift, useEVP, useJFNK, useLSR, usePicard
+from seaice_size import recip_dxC, recip_dyC
+
 
 from seaice_freedrift import freedrift_solver
 from seaice_evp import evp_solver
 from seaice_implicit_solver import picard_solver, jfnk_solver
 from seaice_lsr import lsr_solver
 from seaice_get_dynforcing import get_dynforcing
-from seaice_ocean_stress import ocean_stress
-
-### input:
-# uIce: zonal ice velocity at south-west B-grid (or c grid? ifdef cgrid) u point? (what does the grid look like) (>0 = from west to east)
-# vIce: meridional ice velocty at south-west B-grid v point (>0 = from south to north)
-# hIceMean: mean ice thickness
-# hSnowMean: mean snow thickness
-# Area: ice cover fraction
-# etaN: ocean surface elevation
-# pLoad: surface pressure
-# SeaIceLoad: load of sea ice on ocean surface
-# useRealFreshWaterFlux: flag for using the sea ice load in the calculation of the ocean surface height
-# uVel: zonal ocean velocity
-# vVel: meridional ocean velocity
-# uWind: zonal wind velocity
-# vWind: meridional wind velocity
-# fu: zonal stress on ocean surface (ice or atmopshere)
-# fv: meridional stress on ocean surface (ice or atmopshere)
-
-### output:
-# uIce: zonal ice velocity
-# vIce: meridional ice velocity
-# fu: zonal stress on ocean surface (ice or atmopshere)
-# fv: meridional stress on ocean surface (ice or atmopshere)
 
 
-def dynsolver(uIce, vIce, uVel, vVel, uWind, vWind, hIceMean,
-                hSnowMean, Area, etaN, pLoad, SeaIceLoad, useRealFreshWaterFlux,
-                fu, fv, secondOrderBC, R_low, myTime, myIter):
+# calculate sea ice mass from ice and snow thickness
+@veros_kernel
+def calc_SeaIceMass(state):
 
-    # set up mass per unit area
-    SeaIceMassC = rhoIce * hIceMean
-
-    # if SEAICEaddSnowMass (true)
-    SeaIceMassC= SeaIceMassC + rhoSnow * hSnowMean
+    # calculate sea ice mass centered around c, u, v points
+    SeaIceMassC = rhoIce * state.variables.hIceMean \
+                + rhoSnow * state.variables.hSnowMean
     SeaIceMassU = 0.5 * ( SeaIceMassC + npx.roll(SeaIceMassC,1,1) )
     SeaIceMassV = 0.5 * ( SeaIceMassC + npx.roll(SeaIceMassC,1,0) )
 
-    # if SEAICE_maskRHS... (false)
+    return KernelOutput(SeaIceMassC = SeaIceMassC,
+                        SeaIceMassU = SeaIceMassU,
+                        SeaIceMassV = SeaIceMassV)
 
-    ##### set up forcing fields #####
+@veros_routine
+def update_SeaIceMass(state):
+    
+    # retrieve sea ice mass centered around c, u, v points and update state object
+    SeaIceMass = calc_SeaIceMass(state)
+    state.variables.update(SeaIceMass)
 
-    # compute surface stresses from wind, ocean and ice velocities
-    tauX, tauY = get_dynforcing(uIce, vIce, uWind, vWind, uVel, vVel)
+# calculate surface forcing from wind 
+@veros_kernel
+def calc_SurfaceForcing(state):
 
-    # compute surface pressure at z = 0:
-    # use actual sea surface height phiSurf for tilt computations
-    phiSurf = gravity * etaN
+    # compute surface stresses from wind and ice velocities
+    tauX, tauY = get_dynforcing(state)
+
+    # calculate forcing by surface stress
+    IceSurfStressX0 = tauX * 0.5 * (state.variables.Area + npx.roll(state.variables.Area,1,1))
+    IceSurfStressY0 = tauY * 0.5 * (state.variables.Area + npx.roll(state.variables.Area,1,0))
+
+    # compute surface pressure at z = 0: #???
+    # calculate actual sea surface height #??? phi = geopotential height?
+    phiSurf = gravity * state.variables.etaN
     if useRealFreshWaterFlux:
-        phiSurf = phiSurf + (pLoad + SeaIceLoad * gravity * seaIceLoadFac
+        phiSurf = phiSurf + (state.variables.pLoad \
+                    + state.variables.SeaIceLoad * gravity * seaIceLoadFac
                              ) * recip_rhoConst
     else:
-        phiSurf = phiSurf + pLoad * recip_rhoConst
-
-    # forcing by surface stress
-    #if SEAICEscaleSurfStress (true)
-    IceSurfStressX0 = tauX * 0.5 * (Area + npx.roll(Area,1,1)) #forcex0 in F
-    IceSurfStressY0 = tauY * 0.5 * (Area + npx.roll(Area,1,0)) #forcey0 in F
+        phiSurf = phiSurf + state.variables.pLoad * recip_rhoConst
 
     # add in tilt
-    #if SEAICEuseTILT (true)
-    IceSurfStressX0 = IceSurfStressX0 \
-        - SeaIceMassU * recip_dxC * ( phiSurf - npx.roll(phiSurf,1,1) )
-    IceSurfStressY0 = IceSurfStressY0 \
-        - SeaIceMassV * recip_dyC * ( phiSurf - npx.roll(phiSurf,1,0) )
+    IceSurfStressX0 = IceSurfStressX0 - state.variables.SeaIceMassU \
+                    * recip_dxC * ( phiSurf - npx.roll(phiSurf,1,1) )
+    IceSurfStressY0 = IceSurfStressY0 - state.variables.SeaIceMassV \
+                    * recip_dyC * ( phiSurf - npx.roll(phiSurf,1,0) )
 
-    #if SEAICEuseDYNAMICS (true)
+    return KernelOutput(IceSurfStressX0 = IceSurfStressX0,
+                        IceSurfStressY0 = IceSurfStressY0)
+
+@veros_routine
+def update_SurfaceForcing(state):
+
+    # retrieve surface forcing and update state object
+    SurfaceForcing = calc_SurfaceForcing(state)
+    state.variables.update(SurfaceForcing)
+
+# calculate ice velocities from surface forcing
+@veros_kernel
+def calc_IceVelocities(state):
+
     if useFreedrift:
-        uIce, vIce = freedrift_solver(hIceMean, uVel, vVel,
-                                    IceSurfStressX0, IceSurfStressY0)
-
-    # #ifdef ALLOW_OBCS
-    # #call OBCS_APPLY_UVICE
-    # #solver
+        uIce, vIce = freedrift_solver(state)
 
     if useEVP:
-        uIce, vIce = evp_solver(
-            uIce, vIce, hIceMean, hSnowMean, Area,
-            uVel, vVel, IceSurfStressX0, IceSurfStressY0,
-            SeaIceMassC, SeaIceMassU, SeaIceMassV, R_low,
-            myTime, myIter)
+        uIce, vIce = evp_solver(state)
 
     if useLSR:
-        uIce, vIce = lsr_solver(
-            uIce, vIce, hIceMean, hSnowMean, Area,
-            uVel, vVel, IceSurfStressX0, IceSurfStressY0,
-            SeaIceMassC, SeaIceMassU, SeaIceMassV, R_low,
-            myTime = myTime, myIter = myIter)
+        uIce, vIce = lsr_solver(state)
 
     if usePicard:
-        uIce, vIce = picard_solver(
-            uIce, vIce, hIceMean, hSnowMean, Area,
-            uVel, vVel, IceSurfStressX0, IceSurfStressY0,
-            SeaIceMassC, SeaIceMassU, SeaIceMassV, R_low,
-            myTime, myIter)
+        uIce, vIce = picard_solver(state)
 
     if useJFNK:
-        uIce, vIce = jfnk_solver(
-            uIce, vIce, hIceMean, hSnowMean, Area,
-            uVel, vVel, IceSurfStressX0, IceSurfStressY0,
-            SeaIceMassC, SeaIceMassU, SeaIceMassV, R_low,
-            myTime, myIter)
+        uIce, vIce = jfnk_solver(state)
 
-    # update stress on ocean surface
-    fu, fv = ocean_stress(uIce, vIce, uVel, vVel, Area, fu, fv)
-
+    # ??? leave in?
     # cap the ice velicity at 0.4 m/s to avoid CFL violations in open
     # water areas (drift of zero thickness ice)
     # uIce = npx.clip(uIce, -0.4, 0.4)
     # vIce = npx.clip(vIce, -0.4, 0.4)
 
+    return KernelOutput(uIce = uIce, vIce = vIce)
 
-    return uIce, vIce, fu, fv
+@veros_routine
+def update_IceVelocities(state):
+
+    # retrieve ice velocities and update state object
+    IceVelocities = calc_IceVelocities(state)
+    state.variables.update(IceVelocities)
