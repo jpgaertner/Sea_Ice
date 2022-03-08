b62eb39609f322eb707634aee8d742ce05c8617f
jpgaertner <jan.gaertner.l@gmail.com>
2022-03-08 13:25:33 +0100

    add ridging

--- model.py
+++ model.py
@@ -4,15 +4,20 @@ runtime_settings.backend = backend
 
 from veros import veros_routine, veros_kernel, KernelOutput
 
+from seaice_reg_ridge import update_clean_up_advection, update_ridging
+from dynamics_routines import update_IceStrength
 from seaice_advection import update_Transport, update_Advection
 from seaice_dynsolver import update_IceVelocities, update_SeaIceMass, update_SurfaceForcing
 from seaice_ocean_stress import update_OceanStress
+from seaice_growth import update_Growth
 
 from seaice_size import *
 
 
 # get initial state
 from initialize import state
+
+
 
 @veros_routine
 def model(state):
@@ -22,6 +27,9 @@ def model(state):
 
     # calculate surface forcing due to wind
     update_SurfaceForcing(state)
+
+    # calculate ice strength
+    update_IceStrength(state)
 
     # calculate ice velocities
     update_IceVelocities(state)
@@ -35,12 +43,22 @@ def model(state):
     # calculate change in sea ice fields due to advection
     update_Advection(state)
 
+    # correct overshoots and other pathological cases after advection
+    update_clean_up_advection(state)
 
+    # cut off ice cover fraction at 1 after advection
+    update_ridging(state)
+
+    # calculate thermodynamic ice growth
+    update_Growth(state)
 
 model(state)
+
 
 import matplotlib.pyplot as plt
 
 plt.pcolormesh(state.variables.hIceMean[olx:-olx,oly:-oly])
 plt.colorbar()
-plt.show()
+#plt.show()
+
+print(type(state.variables.hIceMean))
