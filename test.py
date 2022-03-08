b9394217fa33b9ff31e44216b2ea2c5643732d18
jpgaertner <jan.gaertner.l@gmail.com>
2022-03-08 13:23:40 +0100

    test

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from seaice_params import backend
 runtime_settings.backend = backend
 
 from initialize import state
+from seaice_dynsolver import update_IceVelocities
 from veros import veros_routine, veros_kernel, KernelOutput
 from veros.core.operators import numpy as npx
 from veros.core.operators import update, at
@@ -28,6 +29,29 @@ def routine(state):
 
     state.variables.uIce = uIce
 
-print(state.variables.uIce)
-routine(state)
-print(state.variables.uIce)
+# print(state.variables.uIce)
+# routine(state)
+# print(state.variables.uIce)
+
+@veros_kernel
+def func():
+    x = 0
+    while(x<100):
+        print(x)
+        x += 1
+
+
+x = npx.arange(10)
+a = (x>4)
+print(a)
+x *= ~a
+print(x)
+
+
+# x = npx.zeros(10)
+# print('type 2', type(x[0]))
+# print(x[0])
+
+# y = update(x, at[:], x)
+# print('type 3', type(y[0]))
+# print(y[0])
