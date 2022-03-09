0d0a1fac17d1ef758c6189120eaab1ee61b34f26
jpgaertner <jan.gaertner.l@gmail.com>
2022-03-09 16:12:54 +0100

    add AreaW,S to state object

--- model.py
+++ model.py
@@ -7,7 +7,8 @@ from veros import veros_routine, veros_kernel, KernelOutput
 from seaice_reg_ridge import update_clean_up_advection, update_ridging
 from dynamics_routines import update_IceStrength
 from seaice_advection import update_Transport, update_Advection
-from seaice_dynsolver import update_IceVelocities, update_SeaIceMass, update_SurfaceForcing
+from seaice_dynsolver import update_AreaWS, update_IceVelocities, \
+            update_SeaIceMass, update_SurfaceForcing
 from seaice_ocean_stress import update_OceanStress
 from seaice_growth import update_Growth
 
@@ -16,7 +17,6 @@ from seaice_size import *
 
 # get initial state
 from initialize import state
-
 
 
 @veros_routine
@@ -31,6 +31,9 @@ def model(state):
     # calculate ice strength
     update_IceStrength(state)
 
+    # calculate sea ice cover fraction centered around u,v points
+    update_AreaWS(state)
+
     # calculate ice velocities
     update_IceVelocities(state)
 
@@ -39,6 +42,15 @@ def model(state):
 
     # calculate transport through tracer cells
     update_Transport(state)
+
+    import matplotlib.pyplot as plt
+
+    # fig, axs = plt.subplots(1,2, figsize=(14,6))
+    # axs[0].pcolormesh(state.variables.uIce[olx:-olx,oly:-oly])
+    # axs[0].set_title('uIce')
+    # axs[1].pcolormesh(state.variables.vIce[olx:-olx,oly:-oly])
+    # axs[1].set_title('vIce')
+    # plt.show()
 
     # calculate change in sea ice fields due to advection
     update_Advection(state)
@@ -52,13 +64,13 @@ def model(state):
     # calculate thermodynamic ice growth
     update_Growth(state)
 
-model(state)
 
+for i in range(5):
+    model(state)
 
 import matplotlib.pyplot as plt
 
 plt.pcolormesh(state.variables.hIceMean[olx:-olx,oly:-oly])
 plt.colorbar()
-#plt.show()
-
-print(type(state.variables.hIceMean))
+plt.title('hIceMean')
+plt.show()
