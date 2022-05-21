2686b5f9b8de34f6abcfc67b05ccb94d43650fa1
jpgaertner <jan.gaertner.l@gmail.com>
2022-05-21 22:24:54 +0200

    nothing

--- test.py
+++ test.py
@@ -1,57 +1,12 @@
 from veros import runtime_settings
-from seaice_params import backend
+backend = 'numpy'
 runtime_settings.backend = backend
+from veros.core.operators import numpy as npx
 
 from initialize import state
-from seaice_dynsolver import update_IceVelocities
-from veros import veros_routine, veros_kernel, KernelOutput
-from veros.core.operators import numpy as npx
-from veros.core.operators import update, at
 
-@veros_routine
-def f(state):
-    state.variables.uIce = state.variables.uIce + 1.2
+from veros_seaice_plugin import main_seaice
 
-@veros_kernel
-def func(state):
-    x = state.variables.uIce
-    x = x + 20
-    uIce = x
-    vIce = x
-    f(state)
-    uIce = uIce + state.variables.uIce
-    return uIce, vIce
-
-@veros_routine
-def routine(state):
-    uIce, vIce = func(state)
-    #result = func(state)
-
-    state.variables.uIce = uIce
-
-# print(state.variables.uIce)
-# routine(state)
-# print(state.variables.uIce)
-
-@veros_kernel
-def func():
-    x = 0
-    while(x<100):
-        print(x)
-        x += 1
-
-
-x = npx.arange(10)
-a = (x>4)
-print(a)
-x *= ~a
-print(x)
-
-
-# x = npx.zeros(10)
-# print('type 2', type(x[0]))
-# print(x[0])
-
-# y = update(x, at[:], x)
-# print('type 3', type(y[0]))
-# print(y[0])
+print(npx.mean(state.variables.uIce))
+main_seaice(state)
+print(npx.mean(state.variables.uIce))
