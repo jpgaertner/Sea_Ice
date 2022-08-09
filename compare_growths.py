3774c0ad312f2cbae25284c5d92bd3f1891d233c
jpgaertner <jan.gaertner.l@gmail.com>
2022-08-09 10:19:50 +0200

    start compare file

---
+++ compare_growths.py
@@ -0,0 +1,8 @@
+
+import sys
+sys.path.insert(1, "/Users/jgaertne/Documents/seaice_plugin/versis/")
+
+from versis_growth_test import days, ice, area, snow, iceTemp as \
+                            vdays, vice, varea, vsnow, viceTemp
+from test_growth import days, ice, area, snow, iceTemp
+
