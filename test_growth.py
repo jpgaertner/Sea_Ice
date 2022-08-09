811e19159e571b5be5d5b2064254d7c01d0612d4
jpgaertner <jan.gaertner.l@gmail.com>
2022-08-09 10:20:31 +0200

    clean up

--- test_growth.py
+++ test_growth.py
@@ -1,26 +1,5 @@
-
-# hIceMean: mean ice thickness [m3/m2] (= hIceActual * Area with hIceActual = Vol_ice / x_len y_len)
-# iceMask: contains geometry of the set up
-# hSnowMean: mean snow thickness [m3/m2]
-# Area: sea ice cover fraction (0 <= Area <= 1)
-# salt: surface salinity of the ocean [g/kg]
-# TIceSnow: ice/ snow surface temp for each category [K]
-# precip: precipition [m/s] (order 1e-6)
-# snowPrecip: snowfall [m/s]
-# evap: evaporation over ocean [m/s] (same order, =0 for now)
-# runoff: runoff into ocean [m/s]   = 0 for now
-# wspeed: wind speed [m/s]
-# theta: potential temperature of the ocean surface [K]
-# Qnet: surface net heat flux out of the ocean (open water or under ice) [W/m2]
-# Qsw: surface shortwave heat flux out of the ocean (open water or under ice) [W/m2]
-# SWDown: shortwave downward radiation [W/m2]
-# LWDown: longwave downward radiation [W/m2]
-# ATemp: atmospheric temperature [K]
-# aqh: atmospheric specific humidity [g/kg]
-
 from veros import runtime_settings
-backend = 'jax' # flag which backend to use (numpy or jax)
-runtime_settings.backend = backend
+runtime_settings.backend = 'jax'
 
 from veros import veros_routine
 from veros.core.operators import numpy as npx
@@ -40,7 +19,7 @@ iceTemp = npx.array([state.variables.TIceSnow[0,0,0]])
 area = npx.array([state.variables.Area[0,0]])
 days = npx.array([0])
 
-timesteps = 30
+timesteps = 30*12
 
 for i in range(timesteps):
 
